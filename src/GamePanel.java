1155672894e9be050a716355c0f244d98717b0af
Rafi Mozumder <rafimozumder@gmail.com>
2023-05-15 01:47:07 +0600

    Updated Food appearing in random places

---
+++ src/GamePanel.java
@@ -0,0 +1,77 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.sql.Time;
+import java.util.Random;
+import java.awt.event.ActionListener;
+public class GamePanel extends JPanel implements ActionListener{
+    
+    static final int SCREEN_WIDTH = 600;
+    static final int SCREEN_HEIGHT = 600;
+    static final int UNIT_SIZE = 25;
+    static final int GAME_UNITS = (SCREEN_WIDTH*SCREEN_HEIGHT)/UNIT_SIZE;
+    static final int DELAY = 75;
+    final int x[] = new int[GAME_UNITS];
+    final int y[] = new int[GAME_UNITS];
+    int bodyParts = 6;
+    int foodEnten;
+    int foodX;
+    int foodY;
+    char direction = 'R';
+    boolean running = false;
+    Timer timer;
+    Random random;
+
+    GamePanel(){
+        random = new Random();
+        this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+        this.setBackground(Color.BLACK);
+        this.setFocusable(true);
+        this.addKeyListener(new MyKeyAdapter());
+        StartGame();
+    }
+    public void StartGame(){
+        newFood();
+        running = true;
+        timer = new Timer(DELAY,this);
+        timer.start();
+    }
+    public void paintComponent(Graphics g){
+        super.paintComponent(g);
+        draw(g);
+    }
+    public void draw(Graphics g){
+        // for (int i = 0; i < SCREEN_HEIGHT/UNIT_SIZE; i++){
+        //     g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
+        //     g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);            
+        // }
+        g.setColor(Color.red);
+        g.fillOval(foodX, foodY, UNIT_SIZE, UNIT_SIZE);
+    }
+    public void newFood(){
+        foodX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
+        foodY = random.nextInt((int)(SCREEN_HEIGHT/UNIT_SIZE))*UNIT_SIZE;
+    }
+    public void move(){
+
+    }
+    public void checkFood(){
+
+    }
+    public void checkCollision(){
+
+    }
+    public void gameOver(Graphics g){
+
+    }
+    @Override
+    public void actionPerformed(ActionEvent e){
+
+    }
+    public class MyKeyAdapter extends KeyAdapter{
+        @Override
+        public void keyPressed(KeyEvent e){
+
+        }
+    }
+}
